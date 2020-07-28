e4f50ab2e8ae1651f4d1bc1c057da505c0934176
Jose del Río <josedelrio85@gmail.com>
2020-07-28 12:52:59 +0200

    added methods tables, describe, itemv2 and put

--- cmd/main.go
+++ cmd/main.go
@@ -37,8 +37,11 @@ func main() {
 	router.Methods(http.MethodPost)
 	subrouter := router.PathPrefix("/dynamo").Subrouter()
 	subrouter.Handle("/test", handler.HandleFunction())
-	subrouter.Handle("/tables", handler.HandleTables())
+	subrouter.Handle("/tables", handler.PrintTables())
+	subrouter.Handle("/describe", handler.DescribeTable())
 	subrouter.Handle("/item", handler.GetItem())
+	subrouter.Handle("/itemv2", handler.GetItemV2())
+	subrouter.Handle("/put", handler.PutItem())
 
 	log.Println("starting web server...")
 	log.Fatal(http.ListenAndServe(":9001", cors.Default().Handler(subrouter)))
--- pkg/client.go
+++ pkg/client.go
@@ -2,9 +2,12 @@ package dynamo_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -30,10 +33,26 @@ func (h *Handler) HandleFunction() http.Handler {
 	})
 }
 
-// HandleTables is a function used to manage all received requests.
-func (h *Handler) HandleTables() http.Handler {
+// HandleTables is a function used to retrive the list of tables
+func (h *Handler) HandleTables() ([]string, error) {
+	tbllist, err := h.AppContext.Db.ListTables(&dynamodb.ListTablesInput{})
+	if err != nil {
+		msg := "Error retrieving tables list"
+		test(err, msg)
+		return nil, err
+	}
+	tables := []string{}
+
+	for _, tbl := range tbllist.TableNames {
+		tables = append(tables, *tbl)
+	}
+	return tables, err
+}
+
+// PrintTables is a function used to print list tables
+func (h *Handler) PrintTables() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tables, err := h.AppContext.Db.ListTables(&dynamodb.ListTablesInput{})
+		tables, err := h.HandleTables()
 		if err != nil {
 			msg := "Error retrieving tables list"
 			test(err, msg)
@@ -41,10 +60,39 @@ func (h *Handler) HandleTables() http.Handler {
 			return
 		}
 		log.Println("Tables: ")
-		for _, table := range tables.TableNames {
-			log.Println(*table)
+		for _, table := range tables {
+			log.Println(table)
 		}
 		responseOk(w)
+	})
+}
+
+// DescribeTable blabla
+func (h *Handler) DescribeTable() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tables, err := h.HandleTables()
+		if err != nil {
+			msg := "Error retrieving tables list"
+			test(err, msg)
+			responseError(w, msg, err)
+			return
+		}
+
+		for _, tbl := range tables {
+			req := &dynamodb.DescribeTableInput{
+				TableName: aws.String(tbl),
+			}
+			result, err := h.AppContext.Db.DescribeTable(req)
+			if err != nil {
+				msg := fmt.Sprintf("Error describe table %s", tbl)
+				test(err, msg)
+				responseError(w, msg, err)
+				return
+			}
+			table := result.Table
+			fmt.Printf("done %s", table)
+		}
+
 	})
 }
 
@@ -56,11 +104,12 @@ type Input struct {
 
 // Item lalalal
 type Item struct {
-	CustomerID             string
-	LastName               string
-	DateOfBirth            string
-	Email                  string
-	IsEligibleForPromotion bool
+	CustomerID             string `json:"customerId"`
+	LastName               string `json:"lastname"`
+	DateOfBirth            string `json:"date_of_birth"`
+	Email                  string `json:"email"`
+	IsEligibleForPromotion bool   `json:"is_eligible"`
+	Test                   string `json:"test"`
 }
 
 // GetItem asdfasdf
@@ -127,7 +176,8 @@ func (h *Handler) GetItemV2() http.Handler {
 		rawdata, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			msg := "Error parsing body to bytes"
-			test(err, msg)
+			e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
+			e.sendAlarm()
 			responseError(w, msg, err)
 			return
 		}
@@ -151,21 +201,95 @@ func (h *Handler) GetItemV2() http.Handler {
 		})
 		if err != nil {
 			msg := "Error retrieving item"
-			test(err, msg)
+			e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
+			e.sendAlarm()
 			responseError(w, msg, err)
 			return
 		}
 
-		item := Item{}
+		// item := Item{}
+		item := make(map[string]interface{})
+
 		if err := dynamodbattribute.UnmarshalMap(result.Item, &item); err != nil {
 			msg := "Error mapping to item"
-			test(err, msg)
+			e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
+			e.sendAlarm()
 			responseError(w, msg, err)
 			return
 		}
 
-		log.Printf("item: %v", item)
+		log.Printf("retrieved item: %v", item)
 
 		responseOk(w)
 	})
+}
+
+// PutItem blabla
+func (h *Handler) PutItem() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// item := Item{}
+		item := make(map[string]interface{})
+
+		rawdata, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			msg := "Error parsing body to bytes"
+			e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
+			e.sendAlarm()
+			responseError(w, msg, err)
+			return
+		}
+
+		if err := json.Unmarshal(rawdata, &item); err != nil {
+			msg := "Error unmarshaling data"
+			e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
+			e.sendAlarm()
+			responseError(w, msg, err)
+			return
+		}
+
+		av, err := dynamodbattribute.MarshalMap(item)
+		if err != nil {
+			msg := "Error marshalling item"
+			e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
+			e.sendAlarm()
+			responseError(w, msg, err)
+			return
+		}
+
+		// Create item in table Movies
+		input := &dynamodb.PutItemInput{
+			Item:      av,
+			TableName: aws.String("demo-customer-info"),
+		}
+
+		_, err = h.AppContext.Db.PutItem(input)
+
+		if err != nil {
+			msg := "Error putting item"
+			e := &errorLogger{msg, http.StatusInternalServerError, err, logError(err)}
+			e.sendAlarm()
+			responseError(w, msg, err)
+			return
+		}
+
+		log.Printf("putted item: %v", item)
+
+		responseOk(w)
+	})
+}
+
+//HelperRandstring lalalal
+func HelperRandstring(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyz" +
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	seededRand := rand.New(
+		rand.NewSource(time.Now().UnixNano()))
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
